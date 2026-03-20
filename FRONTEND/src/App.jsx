dbe2073853b66f1b793883309c9fe7833a69032b
Jyothish T M <jothishtm2002@gmail.com>
2026-03-20 23:13:31 +0530

    feat: setup routing for registration and OTP pages

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Register from './components/Register'
+import GenerateOtp from './components/GenerateOtp'
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path='/' element={<Register></Register>}></Route>
+          <Route path='/generate-otp' element={<GenerateOtp></GenerateOtp>}></Route>
         </Routes>
       </Router>
     </div>

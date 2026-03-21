45a09ad80d500a4e85c8a03ca2c5d6021bdbfa54
Jyothish T M <jothishtm2002@gmail.com>
2026-03-21 20:47:21 +0530

    feat: implement OTP verification UI and integrate verify API

---
+++ FRONTEND/src/components/VerifyOtp.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+
+
+
+const VerifyOtp = () => {
+
+    const [otp, setOtp] = useState('')
+    const [message, setMessage] = useState('')
+    const location=useLocation('')
+    const email=location.state?.email
+
+    const handleSubmit = async () => {
+        e.preventDefault()
+        try {
+            const response = await axios.post(`http://localhost:5000/auth/verify-otp`,{email,otp})
+            if(response.status==200){
+                setMessage(response.data.message) 
+            }
+        } catch (e) {
+            setMessage('err', e)
+        }
+    }
+    return (
+        <div>
+            <h2>verify otp</h2>
+
+            <form onSubmit={handleSubmit} >
+                <label >otp</label>
+                <input type="text" value={otp} onChange={(e) => (e.target.value)} required />
+                <button type="submit">verify otp</button>
+
+            </form>
+            {message && <p>{message}</p>}
+        </div>
+    )
+}
+
+export default VerifyOtp
+
+
+
+
+
